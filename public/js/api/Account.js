6f4815053bf872c22300632b4b204c501e289016
Angelina <a.kasymova@list.ru>
2024-02-10 20:30:17 +0300

    Реализована функция createRequest, классы Entity, Account, Transaction

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -1,13 +1,8 @@
-/**
- * Класс Account наследуется от Entity.
- * Управляет счетами пользователя.
- * Имеет свойство URL со значением '/account'
- * */
 class Account extends Entity {
-  /**
-   * Получает информацию о счёте
-   * */
-  static get(id = '', callback){
+  static URL = '/account'; 
 
+  static get(id = '', callback){
+    const url = `${this.URL}/${id}`;
+    this.createRequest('GET', url, null, callback);
   }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,28 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    
+    let url = options.url; 
+    let data = Object.entries(options.data);
+    let method = options.method;
+    let formData = new FormData();
+
+    xhr.addEventListener('load', ()=> {
+        options.callback(err, xhr.response)
+    })
+
+    if (method === 'GET'){
+        for(const [key, value] of data) 
+            url += key + '=' + value + '&';
+        }
+    } else {
+        for(const [key, value] of data) {
+            formData.append(key, value);
+        }
+    }
+
+    xhr.open(method, url);
+    xhr.send(formData);
 
 };
+
